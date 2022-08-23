35a4d180228959691731ed0647fe877f9545ce2c
gglyptodon <janina.mass@gmail.com>
2022-08-23 12:34:49 +0200

    add some cli tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,17 +5,6 @@ type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 const PRG: &str = "wakeup";
 
-fn run(args: &[&str], expected: &str) -> TestResult {
-    let cmd = Command::cargo_bin(PRG)?.args(args).assert().success();
-    let out = cmd.get_output();
-    let stdout = String::from_utf8(out.stdout.clone())?;
-    let stderr = String::from_utf8(out.stderr.clone())?;
-    let mut lines: Vec<&str> = stdout.split("\n").filter(|s| !s.is_empty()).collect();
-    println!("{:?} - {:?}", lines, expected);
-    //assert_eq!(lines, expected);
-    Ok(())
-}
-
 fn run_fail_contains(args: &[&str], expected: &str) -> TestResult {
     Command::cargo_bin(PRG)?
         .args(args)
@@ -53,17 +42,3 @@ fn test_dies_called_with_invalid_port()->TestResult {
      let expected = "invalid digit found in string";
     run_fail_contains(&["-m", "aa:bb:cc:dd:ee:ff", "-i", "127.0.0.1", "-p", "a"], expected)
 }
-
-//config
-fn test_dies_no_config_called_with_hostname() {}
-fn test_ok_no_config_called_with_mac() {}
-fn test_dies_no_mac_in_config_called_with_hostname() {}
-fn test_dies_invalid_mac_in_config_called_with_hostname() {}
-
-// os and config
-#[cfg(linux)]
-fn test_finds_config_linux() {}
-#[cfg(windows)]
-fn test_finds_config_windows() {}
-#[cfg(macos)]
-fn test_finds_config_macos() {}
